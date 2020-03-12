7563367780cb25f31c95e2128ce33994213b4323
ChandrikaKothapalli <mchandrika.kothapalli@gmail.com>
2020-03-12 15:45:52 +0530

    Updates

---
+++ precache-manifest.17695e8668901032762134941a757586.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cbe923403fd8aa10a5eefb358eae7c80",
+    "url": "/project2/index.html"
+  },
+  {
+    "revision": "d5cf8031f753e53bf1d8",
+    "url": "/project2/static/css/main.7573618d.chunk.css"
+  },
+  {
+    "revision": "6357e287429b5cb2f37a",
+    "url": "/project2/static/js/2.8411d881.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/project2/static/js/2.8411d881.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d5cf8031f753e53bf1d8",
+    "url": "/project2/static/js/main.74995534.chunk.js"
+  },
+  {
+    "revision": "1796f4669bb73b58fa0f",
+    "url": "/project2/static/js/runtime-main.89b8a379.js"
+  },
+  {
+    "revision": "47e603ff607ad2dcb344a04923966ffa",
+    "url": "/project2/static/media/smile.47e603ff.svg"
+  }
+]);
